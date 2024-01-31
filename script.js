a32cabe8cb1d8770f24cc5cecb0ef4adb2ab549f
Newton Ombese <newtonombese1@gmail.com>
2024-01-31 10:26:28 +0300

    Setting up my JavaScript code

---
+++ script.js
@@ -0,0 +1,20 @@
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+let carouselDom = document.querySelector('.carousel');
+let listItemDom = document.querySelector('.carousel .list');
+let thumbnailDom = document.querySelector('.carousel .thumbnail');
+
+nextDom.onclick = function() {
+  showSlider('next');
+}
+
+function showSlider(type) {
+  let itemSlider = document.querySelectorAll('.carousel .list .item');
+  let itemThumbnail = document.querySelector('.carousel .thumbnail .item');
+
+  if (type === 'next') {
+    listItemDom.appendChild(itemSlider[0]);
+    thumbnailDom.appendChild(itemThumbnail[0]);
+    carouselDom.classList.add('next');
+  }
+}
