322c10c2c29a5927dd44ee552890aa0dd295aa1e
Newton Ombese <newtonombese1@gmail.com>
2024-01-31 10:54:11 +0300

    Setting up the previous slider button

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ nextDom.onclick = function() {
   showSlider('next');
 }
 
+prevDom.onclick = function() {
+  showSlider('prev');
+}
+
+let timeRunning = 3000;
+let runTimeOut;
+
 function showSlider(type) {
   let itemSlider = document.querySelectorAll('.carousel .list .item');
   let itemThumbnail = document.querySelectorAll('.carousel .thumbnail .item');
@@ -16,5 +23,15 @@ function showSlider(type) {
     listItemDom.appendChild(itemSlider[0]);
     thumbnailDom.appendChild(itemThumbnail[0]);
     carouselDom.classList.add('next');
+  } else {
+    let positionLastItem = itemSlider.length - 1;
+    listItemDom.prepend(itemSlider[positionLastItem]);
+    thumbnailDom.prepend(itemThumbnail[positionLastItem]);
+    carouselDom.classList.add('prev');
   }
+
+  clearTimeout(runTimeOut);
+  runTimeOut = setTimeout(() => {
+    carouselDom.classList.remove('next');
+  }, timeRunning)
 }
